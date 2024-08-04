93f2b61e66deb8a34191040f306edb4d1271d1f8
Ankur verma <ankurverma7707@gmail.conm>
2024-08-04 21:15:03 +0530

    new

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+   
+  return (
+    <>
+       <div className='text-blue-800 text-[23px] m-auto flex justify-center' >
+          <h1>
+            Hello CypherSync
+          </h1>
+       </div>
+    </>
+  )
+}
+
+export default App
